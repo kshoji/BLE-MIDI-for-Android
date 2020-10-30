b14039de063132aba0336f41578749fdaf3661cd
Kaoru Shoji <0x0badc0de@gmail.com>
2020-10-30 09:15:50 +0900

    Reverted a necessary code.

--- sample/src/main/java/jp/kshoji/blemidi/sample/CentralActivity.java
+++ sample/src/main/java/jp/kshoji/blemidi/sample/CentralActivity.java
@@ -574,6 +574,9 @@ public class CentralActivity extends Activity {
                 }
             }
         });
+
+        // scan devices for 30 seconds
+        bleMidiCentralProvider.startScanDevice(30000);
     }
 
     @Override
