5cb3aa8ae755f33d305293bd033cb3d1f614495b
Kaoru Shoji <0x0badc0de@gmail.com>
2024-04-12 20:00:41 +0900

    Remove unnecessary code

--- BLE-MIDI-library/src/main/java/jp/kshoji/unity/midi/BleMidiUnityPlayerActivity.java
+++ BLE-MIDI-library/src/main/java/jp/kshoji/unity/midi/BleMidiUnityPlayerActivity.java
@@ -24,14 +24,14 @@ public class BleMidiUnityPlayerActivity extends UnityPlayerActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == BleUtils.SELECT_DEVICE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
                 try {
                     AssociationInfo associationInfo = data.getParcelableExtra(CompanionDeviceManager.EXTRA_ASSOCIATION, AssociationInfo.class);
                     bleMidiCentralProvider.connectGatt(associationInfo.getAssociatedDevice().getBleDevice().getDevice());
                 } catch (Throwable t) {
                     Log.d(Constants.TAG, t.getMessage(), t);
                 }
-            } else if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 try {
                     ScanResult scanResult = data.getParcelableExtra(CompanionDeviceManager.EXTRA_DEVICE);
                     bleMidiCentralProvider.connectGatt(scanResult.getDevice());
